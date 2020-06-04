e7a14b0619c4079db7509c87a8061b8b6eac05e9
Kartikey Tanna <tannakartikey@gmail.com>
2020-06-04 23:33:18 +0530

    #1 An endpoint to create an Issue

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import bodyParser from 'body-parser'
+import { Request, Response } from 'express' //eslint-disable-line
+import { Application, GitHubAPI } from 'probot' //eslint-disable-line
 
-export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+export = async (app: Application) => {
+  const router = app.route('/make-in-india-bot')
+
+  router.use(bodyParser.json())
+
+  router.post('/product', async (req: Request, res: Response) => {
+    const { title, body }: { title: string; body: string } = req.body
+    const installationToken: string = await app.app.getInstallationAccessToken({
+      installationId: parseInt(process.env.INSTALLATION_ID, 10)
+    })
+    const github = GitHubAPI({
+      auth: installationToken,
+      logger: app.log.child({ name: 'make-in-india-bot' })
+    })
+
+    try {
+      await github.issues.create({
+        body,
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO,
+        title
+      })
+    } catch (err) {
+      return res.send({ status: 'error', errors: err })
+    }
+
+    return res.status(200).end()
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
